210b0229a590bc236052d87041c42b11e88c47d3
Eldos <eldos.baktybekov@gmail.com>
2022-11-11 19:36:55 +0600

    :lipstick: Add instruction view, update menu view

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,3 +1,4 @@
 from .faq import *
+from .instruction import *
 from .menu import *
 from .news_channel import *
--- src/views/menu.py
+++ src/views/menu.py
@@ -1,10 +1,26 @@
+import models
 from views.base import BaseView
 
 from keyboards import MenuMarkup
+from services.subscription import is_subscription_active
 
 __all__ = ('MenuView',)
 
 
 class MenuView(BaseView):
-    text = 'menu'
     reply_markup = MenuMarkup()
+
+    def __init__(self, user: models.User):
+        self.__user = user
+
+    def get_text(self) -> str:
+        lines = []
+        if not is_subscription_active(self.__user.subscribed_at, self.__user.subscription_expire_at):
+            return '❗️ У вас нету активной подписки'
+        if self.__user.is_trial_period:
+            lines.append('❗️ У вас пробный период')
+        lines.append(
+            f'Подписка активирована: {self.__user.subscribed_at:%H:%M %d.%m.%Y}'
+            f'\nАктивна до: {self.__user.subscription_expire_at:%H:%M %d.%m.%Y}'
+        )
+        return '\n'.join(lines)
