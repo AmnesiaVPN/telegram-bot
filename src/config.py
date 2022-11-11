ca22ae8c8e943d27b3879ad80bc4364d14f5fc83
Eldos <eldos.baktybekov@gmail.com>
2022-11-11 19:00:28 +0600

    :wrench: Add server api base url config

--- src/config.py
+++ src/config.py
@@ -15,8 +15,14 @@ class BotConfig:
 
 
 @dataclass(frozen=True, slots=True)
+class ServerAPIConfig:
+    base_url: str
+
+
+@dataclass(frozen=True, slots=True)
 class Config:
     bot: BotConfig
+    server_api: ServerAPIConfig
 
 
 def load_config(file_path: str | pathlib.Path) -> Config:
@@ -24,9 +30,13 @@ def load_config(file_path: str | pathlib.Path) -> Config:
     config.read(file_path)
 
     bot_config = config['telegram_bot']
+    server_api_config = config['server_api']
 
     return Config(
         bot=BotConfig(
             token=bot_config.get('token'),
+        ),
+        server_api=ServerAPIConfig(
+            base_url=server_api_config.get('base_url'),
         ),
     )
