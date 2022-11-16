e3978e5d8a43a639c8adab7d0b33b94b7f931b9c
Eldos <eldos.baktybekov@gmail.com>
2022-11-16 10:54:19 +0600

    :fire: Remove useless code

--- src/services/subscription.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def is_subscription_active(starts_at: datetime.datetime, ends_at: datetime.datetime) -> bool:
-    starts_at = starts_at.replace(tzinfo=None)
-    ends_at = ends_at.replace(tzinfo=None)
-    return starts_at <= datetime.datetime.utcnow() <= ends_at
