2c18f245a72167f0d0569af9329b9cade3b2e53a
Eldos <eldos.baktybekov@gmail.com>
2022-11-17 22:56:58 +0600

    :sparkles: Add instructions if user is subscribed

--- src/handlers/subscription.py
+++ src/handlers/subscription.py
@@ -5,7 +5,7 @@ from aiogram.types import Message
 from config import Config
 from services.server_api import ServerAPIClient, deleting_temporary_config_file
 from shortcuts import answer_view
-from views import MenuView, PaymentMenuView
+from views import MenuView, PaymentMenuView, InstructionView
 
 __all__ = ('register_handlers',)
 
@@ -17,6 +17,7 @@ async def on_user_start(message: Message, server_api_client: ServerAPIClient, co
         await answer_view(message, PaymentMenuView(message.from_user.id, config.donationalerts.payment_page_url))
         return
     user_config_text = await server_api_client.get_user_config(message.from_user.id)
+    await answer_view(message, InstructionView())
     with deleting_temporary_config_file(message.from_user.id, user_config_text) as user_config:
         await message.answer_document(user_config)
 
