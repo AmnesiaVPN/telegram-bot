525b8ed4581bda2616146151d4bd6176cfdb302c
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 16:51:49 +0600

    :pencil2: Fix typo

--- src/models/users.py
+++ src/models/users.py
@@ -7,7 +7,7 @@ __all__ = ('User',)
 
 class User(BaseModel):
     telegram_id: int
-    subscription_expire_at: datetime.datetime
+    subscription_expires_at: datetime.datetime
     subscribed_at: datetime.datetime
     is_trial_period: bool
     is_subscribed: bool
