530c96d8e322d64eb6e220e3f817fea16db11b3c
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 16:51:18 +0600

    :fire: Remove unused code

--- src/views/promocode.py
+++
@@ -1,9 +0,0 @@
-from views.base import BaseView
-from keyboards import PromocodeSkipMarkup
-
-__all__ = ('PromocodeView',)
-
-
-class PromocodeView(BaseView):
-    text = 'Введите ваш промокод если он у вас есть'
-    reply_markup = PromocodeSkipMarkup()
