4066949996557e3cd29279ea1d6089491e6543bf
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 16:52:06 +0600

    :sparkles: Add custom filter

---
+++ src/filters/message_length.py
@@ -0,0 +1,17 @@
+from aiogram.dispatcher.filters import BoundFilter
+from aiogram.types import Message
+
+__all__ = ('MessageLengthFilter',)
+
+
+class MessageLengthFilter(BoundFilter):
+    key = 'message_length'
+
+    __slots__ = ('__max_length',)
+
+    def __init__(self, *, min_length: int | None = None, max_length: int | None = None):
+        self.__min_length = min_length or 1
+        self.__max_length = max_length or 4096
+
+    async def check(self, message: Message) -> bool:
+        return self.__min_length <= len(message.text) <= self.__max_length
