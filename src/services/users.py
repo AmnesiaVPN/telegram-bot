38a1f7bbdff54f9e70936c90bd85259f9724153a
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 16:53:59 +0600

    :goal_net: Catch error when promocode does not exist

--- src/services/users.py
+++ src/services/users.py
@@ -2,8 +2,8 @@ from typing import Callable
 
 import httpx
 
-import exceptions
 import models
+from core import exceptions
 
 __all__ = ('UsersAPIService',)
 
@@ -30,12 +30,6 @@ class UsersAPIService:
         elif response.status_code == 404:
             raise exceptions.UserNotFoundError
 
-    async def get_or_create(self, telegram_id: int) -> tuple[models.User, bool]:
-        try:
-            return await self.get_by_telegram_id(telegram_id), False
-        except exceptions.UserNotFoundError:
-            return await self.create(telegram_id), True
-
     async def get_config(self, telegram_id: int) -> str:
         url = f'/users/{telegram_id}/config/'
         async with self.__http_client_factory() as client:
@@ -49,5 +43,10 @@ class UsersAPIService:
         request_body = {'promocode': promocode}
         async with self.__http_client_factory() as client:
             response = await client.post(url, json=request_body)
-        response_body = response.json()
-        return models.PromocodeActivated.parse_obj(response_body)
+        if response.status_code == 200:
+            response_body = response.json()
+            return models.PromocodeActivated.parse_obj(response_body)
+        elif response.status_code == 404:
+            response_body = response.json()
+            if response_body['message'] == 'Promocode is not found':
+                raise exceptions.PromocodeNotFoundError
