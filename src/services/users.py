93b7bc0a949a9d839e2612bb812705a36759be35
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 15:35:37 +0600

    :recycle: Refactor repository to api service

--- src/services/users.py
+++ src/services/users.py
@@ -1,14 +1,20 @@
-import models
+from typing import Callable
+
+import httpx
+
 import exceptions
-from repositories.base import Repository
+import models
 
-__all__ = ('UserRepository',)
+__all__ = ('UsersAPIService',)
 
 
-class UserRepository(Repository):
+class UsersAPIService:
+
+    def __init__(self, http_client_factory: Callable[[], httpx.AsyncClient]):
+        self.__http_client_factory = http_client_factory
 
     async def create(self, telegram_id: int) -> models.User:
-        async with self._http_client_factory() as client:
+        async with self.__http_client_factory() as client:
             response = await client.post('/users/', json={'telegram_id': telegram_id})
         if response.status_code == 200:
             return models.User.parse_obj(response.json())
@@ -17,7 +23,7 @@ class UserRepository(Repository):
 
     async def get_by_telegram_id(self, telegram_id: int) -> models.User:
         url = f'/users/{telegram_id}/'
-        async with self._http_client_factory() as client:
+        async with self.__http_client_factory() as client:
             response = await client.get(url)
         if response.status_code == 200:
             return models.User.parse_obj(response.json())
@@ -32,8 +38,16 @@ class UserRepository(Repository):
 
     async def get_config(self, telegram_id: int) -> str:
         url = f'/users/{telegram_id}/config/'
-        async with self._http_client_factory() as client:
+        async with self.__http_client_factory() as client:
             response = await client.get(url)
         if response.is_error:
             raise Exception
         return response.text
+
+    async def activate_promocode(self, telegram_id: int, promocode: str) -> models.PromocodeActivated:
+        url = f'/users/{telegram_id}/promocodes/'
+        request_body = {'promocode': promocode}
+        async with self.__http_client_factory() as client:
+            response = await client.post(url, json=request_body)
+        response_body = response.json()
+        return models.PromocodeActivated.parse_obj(response_body)
